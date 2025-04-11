ff790195f31a753444f4fbc7664e2aed84b5b13b
yincong <yincong@baidu.com>
2025-04-11 15:08:51 +0800

    add aws

---
+++ demo/client/aws/aws.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/yincongcyincong/mcp-client-go/clients"
+	"github.com/yincongcyincong/mcp-client-go/clients/aws"
+	"github.com/yincongcyincong/mcp-client-go/clients/param"
+	"log"
+	"time"
+)
+
+func main() {
+	mc := aws.InitAwsMCPClient("xxx", "xxx", "xxx", "", nil, nil, nil)
+
+	// Create context with timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	errs := clients.RegisterMCPClient(ctx, []*param.MCPClientConf{mc})
+	if len(errs) > 0 {
+		log.Fatal("InitMCPClient failed:", errs)
+	}
+
+	c, err := clients.GetMCPClient(aws.NpxAwsMcpServer)
+	if err != nil {
+		log.Fatal("GetMCPClient failed:", err)
+	}
+
+	for _, tool := range c.Tools {
+		toolByte, _ := json.Marshal(tool)
+		fmt.Println(string(toolByte))
+	}
+
+	data, err := c.ExecTools(ctx, "retrieve_from_aws_kb", map[string]interface{}{
+		"query":           "xxx",
+		"knowledgeBaseId": "xxx",
+		"n":               3,
+	})
+	if err != nil {
+		log.Fatal("ExecTools failed:", err)
+	}
+
+	fmt.Println(data)
+
+}
