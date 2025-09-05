659047ab3d14eeb275d5a87ece2a1fa172d4c319
黄孟柱 <7427565+huang_meng_zhu@user.noreply.gitee.com>
2025-09-05 11:56:52 +0800

    兼容原来用法的情况下, 对clients的client对象实现隔离

--- clients/client.go
+++ clients/client.go
@@ -4,13 +4,11 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"runtime/debug"
-	"sync"
 	"time"
-	
+
 	"github.com/mark3labs/mcp-go/client"
 	"github.com/mark3labs/mcp-go/client/transport"
 	"github.com/mark3labs/mcp-go/mcp"
@@ -19,7 +17,7 @@ import (
 )
 
 var (
-	mcpClients sync.Map
+	defaultClients Clients
 )
 
 type MCPClient struct {
@@ -27,9 +25,10 @@ type MCPClient struct {
 	Client  *client.Client
 	InitReq *mcp.InitializeResult
 	Tools   []mcp.Tool
-	
+
 	ctx    context.Context
 	cancel context.CancelFunc
+	parent *Clients
 }
 
 // InitByConfFile Initialize multiple MCP clients from configuration files
@@ -38,13 +37,13 @@ func InitByConfFile(configFilePath string) ([]*param.MCPClientConf, error) {
 	if err != nil {
 		return nil, err
 	}
-	
+
 	config := new(param.McpClientGoConfig)
 	err = json.Unmarshal(data, config)
 	if err != nil {
 		return nil, err
 	}
-	
+
 	mcs := make([]*param.MCPClientConf, 0)
 	for mcpName, mcpConf := range config.McpServers {
 		mcpClientConf := GetOneMCPClient(mcpName, mcpConf)
@@ -52,16 +51,16 @@ func InitByConfFile(configFilePath string) ([]*param.MCPClientConf, error) {
 			mcs = append(mcs, mcpClientConf)
 		}
 	}
-	
+
 	return mcs, nil
-	
+
 }
 
 func GetOneMCPClient(mcpName string, mcpConf *param.MCPConfig) *param.MCPClientConf {
 	if mcpConf.Disabled {
 		return nil
 	}
-	
+
 	mcpType := getMcpType(mcpConf)
 	switch mcpType {
 	case param.StdioType:
@@ -74,7 +73,7 @@ func GetOneMCPClient(mcpName string, mcpConf *param.MCPConfig) *param.MCPClientC
 			log.Println("CheckSSEOrHTTP fail, err:", err)
 			return nil
 		}
-		
+
 		if httpType == param.SSEType {
 			return InitSSEMCPClient(mcpName, mcpConf.Url,
 				param.WithSSEOptions(transport.WithHeaders(mcpConf.Headers)),
@@ -85,195 +84,17 @@ func GetOneMCPClient(mcpName string, mcpConf *param.MCPConfig) *param.MCPClientC
 				param.WithDescription(mcpConf.Description),
 				param.WithHttpOauth(mcpConf.OAuth))
 		}
-	
+
 	default:
 		log.Println("mcp type not exist, mcpType:", mcpType)
 	}
-	
-	return nil
-}
 
-// RegisterMCPClient register multiple MCP clients
-func RegisterMCPClient(ctx context.Context, params []*param.MCPClientConf) map[string]error {
-	errs := make(map[string]error)
-	
-	wg := sync.WaitGroup{}
-	mutex := sync.Mutex{}
-	
-	for _, clientParam := range params {
-		wg.Add(1)
-		go func(cp *param.MCPClientConf) {
-			defer func() {
-				if r := recover(); r != nil {
-					log.Printf("panic recovered: %v\n%s", r, debug.Stack())
-				}
-				wg.Done()
-			}()
-			if cp.SSEClientConf != nil && cp.ClientType == param.SSEType {
-				err := createSSEMCPClient(ctx, cp)
-				if err != nil {
-					mutex.Lock()
-					errs[clientParam.Name] = err
-					mutex.Unlock()
-				}
-			} else if cp.HTTPStreamerConf != nil && cp.ClientType == param.HTTPStreamer {
-				err := createHTTPStreamCPClient(ctx, cp)
-				if err != nil {
-					mutex.Lock()
-					errs[clientParam.Name] = err
-					mutex.Unlock()
-				}
-			} else {
-				err := createStdioMCPClient(ctx, cp)
-				if err != nil {
-					mutex.Lock()
-					errs[clientParam.Name] = err
-					mutex.Unlock()
-				}
-				
-			}
-		}(clientParam)
-	}
-	wg.Wait()
-	
-	return errs
-}
-
-// createStdioMCPClient create stdio MCP client
-func createSSEMCPClient(ctx context.Context, clientParam *param.MCPClientConf) error {
-	c, err := client.NewSSEMCPClient(
-		clientParam.SSEClientConf.BaseUrl,
-		clientParam.SSEClientConf.Options...,
-	)
-	if err != nil {
-		return err
-	}
-	
-	err = c.Start(context.Background())
-	if err != nil {
-		return err
-	}
-	
-	initResult, err := c.Initialize(ctx, clientParam.SSEClientConf.InitReq)
-	if err != nil {
-		return err
-	}
-	
-	return initMCPClient(ctx, c, clientParam, initResult)
-}
-
-// createSSEMCPClient create SSE MCP client
-func createHTTPStreamCPClient(ctx context.Context, clientParam *param.MCPClientConf) error {
-	var c *client.Client
-	var err error
-	if clientParam.HTTPStreamerConf.Oauth != nil {
-		c, err = client.NewOAuthStreamableHttpClient(
-			clientParam.HTTPStreamerConf.BaseURL,
-			*clientParam.HTTPStreamerConf.Oauth,
-			clientParam.HTTPStreamerConf.Options...,
-		)
-	} else {
-		c, err = client.NewStreamableHttpClient(
-			clientParam.HTTPStreamerConf.BaseURL,
-			clientParam.HTTPStreamerConf.Options...,
-		)
-	}
-	
-	if err != nil {
-		return err
-	}
-	
-	err = c.Start(context.Background())
-	if err != nil {
-		return err
-	}
-	
-	initResult, err := c.Initialize(ctx, clientParam.HTTPStreamerConf.InitReq)
-	if err != nil {
-		return err
-	}
-	
-	return initMCPClient(ctx, c, clientParam, initResult)
-}
-
-// createStdioMCPClient create stdio MCP client
-func createStdioMCPClient(ctx context.Context, clientParam *param.MCPClientConf) error {
-	c, err := client.NewStdioMCPClient(
-		clientParam.StdioClientConf.Command,
-		clientParam.StdioClientConf.Env,
-		clientParam.StdioClientConf.Args...,
-	)
-	if err != nil {
-		return err
-	}
-	
-	go func() {
-		defer func() {
-			if r := recover(); r != nil {
-				log.Printf("panic recovered: %v\n%s", r, debug.Stack())
-			}
-		}()
-		stderr, ok := client.GetStderr(c)
-		if !ok {
-			return
-		}
-		
-		buf := make([]byte, 1024) // 合理大小缓存区
-		for {
-			n, err := stderr.Read(buf)
-			if err != nil {
-				if err == io.EOF {
-					log.Println("stderr closed:", clientParam.Name)
-					break
-				}
-				log.Println("read stderr fail:", clientParam.Name, err)
-				break
-			}
-			if n > 0 {
-				log.Println("stderr:", clientParam.Name, string(buf[:n]))
-			}
-		}
-	}()
-	
-	initResult, err := c.Initialize(ctx, clientParam.StdioClientConf.InitReq)
-	if err != nil {
-		return err
-	}
-	
-	return initMCPClient(ctx, c, clientParam, initResult)
-}
-
-func initMCPClient(ctx context.Context, c *client.Client,
-	clientParam *param.MCPClientConf, initResult *mcp.InitializeResult) error {
-	
-	mcContext, cancel := context.WithCancel(context.Background())
-	mc := &MCPClient{
-		Conf:    clientParam,
-		Client:  c,
-		InitReq: initResult,
-		
-		ctx:    mcContext,
-		cancel: cancel,
-	}
-	tools, err := mc.GetAllTools(ctx, "")
-	if err != nil {
-		return err
-	}
-	mc.Tools = tools
-	
-	if clientInter, ok := mcpClients.Load(clientParam.Name); ok {
-		clientInter.(*MCPClient).Close()
-	}
-	
-	mcpClients.Store(clientParam.Name, mc)
-	
-	go mc.handlePing()
 	return nil
 }
 
 // InitStdioMCPClient Initialize the stdio MCP client
 func InitStdioMCPClient(name, command string, env, args []string, options ...param.Option) *param.MCPClientConf {
-	
+
 	mcpClient := &param.MCPClientConf{
 		Name:       name,
 		ClientType: param.StdioType,
@@ -283,28 +104,28 @@ func InitStdioMCPClient(name, command string, env, args []string, options ...par
 			Args:    args,
 		},
 	}
-	
+
 	for _, o := range options {
 		o(mcpClient)
 	}
-	
+
 	if mcpClient.StdioClientConf.InitReq.Params.ProtocolVersion == "" {
 		mcpClient.StdioClientConf.InitReq.Params.ProtocolVersion = mcp.LATEST_PROTOCOL_VERSION
 	}
-	
+
 	if mcpClient.StdioClientConf.InitReq.Params.ClientInfo.Name == "" {
 		mcpClient.StdioClientConf.InitReq.Params.ClientInfo = mcp.Implementation{
 			Name:    "mcp-server/unknown",
 			Version: "0.1.0",
 		}
 	}
-	
+
 	return mcpClient
 }
 
 // InitSSEMCPClient Initialize the SSE MCP client
 func InitSSEMCPClient(name, baseUrl string, options ...param.Option) *param.MCPClientConf {
-	
+
 	mcpClient := &param.MCPClientConf{
 		Name:       name,
 		ClientType: param.SSEType,
@@ -312,22 +133,22 @@ func InitSSEMCPClient(name, baseUrl string, options ...param.Option) *param.MCPC
 			BaseUrl: baseUrl,
 		},
 	}
-	
+
 	for _, o := range options {
 		o(mcpClient)
 	}
-	
+
 	if mcpClient.SSEClientConf.InitReq.Params.ProtocolVersion == "" {
 		mcpClient.SSEClientConf.InitReq.Params.ProtocolVersion = mcp.LATEST_PROTOCOL_VERSION
 	}
-	
+
 	if mcpClient.SSEClientConf.InitReq.Params.ClientInfo.Name == "" {
 		mcpClient.SSEClientConf.InitReq.Params.ClientInfo = mcp.Implementation{
 			Name:    "mcp-server/unknown",
 			Version: "0.1.0",
 		}
 	}
-	
+
 	return mcpClient
 }
 
@@ -340,64 +161,34 @@ func InitHttpMCPClient(name, baseUrl string, options ...param.Option) *param.MCP
 			BaseURL: baseUrl,
 		},
 	}
-	
+
 	for _, o := range options {
 		o(mcpClient)
 	}
-	
+
 	if mcpClient.HTTPStreamerConf.InitReq.Params.ProtocolVersion == "" {
 		mcpClient.HTTPStreamerConf.InitReq.Params.ProtocolVersion = mcp.LATEST_PROTOCOL_VERSION
 	}
-	
+
 	if mcpClient.HTTPStreamerConf.InitReq.Params.ClientInfo.Name == "" {
 		mcpClient.HTTPStreamerConf.InitReq.Params.ClientInfo = mcp.Implementation{
 			Name:    "mcp-server/unknown",
 			Version: "0.1.0",
 		}
 	}
-	
+
 	return mcpClient
-}
-
-// GetMCPClient Get MCP client by name
-func GetMCPClient(name string) (*MCPClient, error) {
-	v, ok := mcpClients.Load(name)
-	if !ok {
-		return nil, fmt.Errorf("client %s not found", name)
-	}
-	return v.(*MCPClient), nil
-}
-
-// GetMCPClientByToolName Get MCP client by tool name
-func GetMCPClientByToolName(toolName string) (*MCPClient, error) {
-	var res *MCPClient
-	mcpClients.Range(func(key, value interface{}) bool {
-		mc := value.(*MCPClient)
-		for _, tool := range mc.Tools {
-			if tool.Name == toolName {
-				res = mc
-				return true
-			}
-		}
-		return true
-	})
-	
-	if res == nil {
-		return nil, fmt.Errorf("tool %s not found", toolName)
-	}
-	
-	return res, nil
 }
 
 // GetAllTools Get all tools from MCP client
 func (m *MCPClient) GetAllTools(ctx context.Context, cursor mcp.Cursor) ([]mcp.Tool, error) {
 	toolsRequest := mcp.ListToolsRequest{}
 	toolsRequest.Params.Cursor = cursor
-	
+
 	var tools *mcp.ListToolsResult
 	var err error
 	tools, err = m.Client.ListTools(ctx, toolsRequest)
-	
+
 	if err != nil {
 		return nil, err
 	}
@@ -407,24 +198,24 @@ func (m *MCPClient) GetAllTools(ctx context.Context, cursor mcp.Cursor) ([]mcp.T
 // ExecTools Execute a tool
 func (m *MCPClient) ExecTools(ctx context.Context, name string, params map[string]interface{}) (string, error) {
 	var useTool *mcp.Tool
-	
+
 	for _, tool := range m.Tools {
 		if tool.Name == name {
 			useTool = &tool
 			break
 		}
 	}
-	
+
 	if useTool == nil {
 		return "", fmt.Errorf("tool %s not found", name)
 	}
-	
+
 	for _, reqParam := range useTool.InputSchema.Required {
 		if _, ok := params[reqParam]; !ok {
 			return "", fmt.Errorf("required parameter %s not found", reqParam)
 		}
 	}
-	
+
 	reqTool := mcp.CallToolRequest{
 		Request: mcp.Request{
 			Method: "tools/call",
@@ -432,7 +223,7 @@ func (m *MCPClient) ExecTools(ctx context.Context, name string, params map[strin
 	}
 	reqTool.Params.Name = name
 	reqTool.Params.Arguments = params
-	
+
 	if _, ok := m.Conf.ToolsBeforeFunc[name]; ok {
 		err := m.Conf.ToolsBeforeFunc[name](&reqTool)
 		if err != nil {
@@ -445,11 +236,11 @@ func (m *MCPClient) ExecTools(ctx context.Context, name string, params map[strin
 	if err != nil {
 		return "", err
 	}
-	
+
 	if _, ok := m.Conf.ToolsAfterFunc[name]; ok {
 		return m.Conf.ToolsAfterFunc[name](result)
 	}
-	
+
 	return utils.ReturnString(result), nil
 }
 
@@ -460,10 +251,10 @@ func (m *MCPClient) handlePing() {
 			log.Printf("panic recovered: %v\n%s", r, debug.Stack())
 		}
 	}()
-	
+
 	ticker := time.NewTicker(10 * time.Second)
 	defer ticker.Stop()
-	
+
 	for {
 		select {
 		case <-ticker.C:
@@ -473,7 +264,7 @@ func (m *MCPClient) handlePing() {
 			return
 		}
 	}
-	
+
 }
 
 // restartMCPServer restart mcp server
@@ -481,26 +272,26 @@ func (m *MCPClient) restartMCPServer() bool {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	err := m.Client.Ping(ctx)
-	
+
 	if err != nil {
 		log.Println("mcp ping fail:", err)
-		
+
 		if m.Conf.SSEClientConf != nil && m.Conf.ClientType == param.SSEType {
-			err = createSSEMCPClient(ctx, m.Conf)
+			err = m.parent.createSSEMCPClient(ctx, m.Conf)
 		} else if m.Conf.HTTPStreamerConf != nil && m.Conf.ClientType == param.HTTPStreamer {
-			err = createHTTPStreamCPClient(ctx, m.Conf)
+			err = m.parent.createHTTPStreamCPClient(ctx, m.Conf)
 		} else {
-			err = createStdioMCPClient(ctx, m.Conf)
+			err = m.parent.createStdioMCPClient(ctx, m.Conf)
 		}
-		
+
 		if err != nil {
 			log.Println("create new mcp client fail:", err)
 			return false
 		}
-		
+
 		return true
 	}
-	
+
 	return false
 }
 
@@ -509,33 +300,15 @@ func (m *MCPClient) Close() {
 	m.Client.Close()
 }
 
-func RemoveMCPClient(name string) error {
-	c, err := GetMCPClient(name)
-	if err != nil {
-		return err
-	}
-	c.Close()
-	mcpClients.Delete(name)
-	return nil
-}
-
-func ClearAllMCPClient() {
-	mcpClients.Range(func(key, value interface{}) bool {
-		value.(*MCPClient).Close()
-		return true
-	})
-	mcpClients.Clear()
-}
-
 // getMcpType get mcp type
 func getMcpType(conf *param.MCPConfig) string {
 	if conf.Type != "" {
 		return conf.Type
 	}
-	
+
 	if conf.Command != "" {
 		return param.StdioConfigType
 	}
-	
+
 	return param.HTTPConfigType
 }
